7fa5b91297859b6247868a36f8ec7a652d0be13e
Alexandre Aubin <alex.aubin@mailoo.org>
2019-02-22 20:11:23 +0100

    Merge app tile stuff together

--- portal/assets/js/global.js
+++ portal/assets/js/global.js
@@ -5,21 +5,10 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   // Variables
-  var liMenu = document.querySelectorAll('#apps a')
-    , colors = ['redbg','purpledarkbg','darkbluebg','orangebg','greenbg','darkbluebg','purpledarkbg','yellowbg','lightpinkbg','pinkbg','turquoisebg','yellowbg','lightbluebg','purpledarkbg', 'bluebg']
+  var colors = ['redbg','purpledarkbg','darkbluebg','orangebg','greenbg','darkbluebg','purpledarkbg','yellowbg','lightpinkbg','pinkbg','turquoisebg','yellowbg','lightbluebg','purpledarkbg', 'bluebg']
     , addMailAlias = document.getElementById('add-mailalias')
     , addMaildrop = document.getElementById('add-maildrop')
   ;
-
-  liMenu && [].forEach.call(liMenu, function(el, i) {
-    // Select a color value from the App label
-    randomColorNumber = parseInt(el.textContent, 36) % colors.length;
-    //randomColorNumber = i%colors.length; // Old value
-    // Add color class.
-    el.classList.add(colors[randomColorNumber]);
-    // Set first-letter data attribute.
-    el.querySelector('.first-letter').setAttribute('data-first-letter',el.textContent.substring(0, 2));
-  });
 
   addMailAlias && addMailAlias.addEventListener('click', function(){
     // Clone last input.
@@ -39,11 +28,22 @@ document.addEventListener('DOMContentLoaded', function() {
     addMaildrop.parentNode.insertBefore(inputDropClone, addMaildrop);
   });
 
-  // handle app links so they work both in plain info page and in the info iframe called from ynhpanel.js
   var app_tiles = document.getElementsByClassName("app-tile");
   if (app_tiles) {
     for (var i = 0; i < app_tiles.length; i++) {
-        app_tiles[i].addEventListener('click', function(event) {
+
+        var el = app_tiles[i];
+
+        // Select a color value from the App label
+        randomColorNumber = parseInt(el.textContent, 36) % colors.length;
+        //randomColorNumber = i%colors.length; // Old value
+        // Add color class.
+        el.classList.add(colors[randomColorNumber]);
+        // Set first-letter data attribute.
+        el.querySelector('.first-letter').setAttribute('data-first-letter', el.textContent.substring(0, 2));
+
+        // handle app links so they work both in plain info page and in the info iframe called from ynhpanel.js
+        el.addEventListener('click', function(event) {
         // if asked to open in new tab
         if (event.ctrlKey || event.shiftKey || event.metaKey
             || (event.button && event.button == 1)) {
