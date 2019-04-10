YunoHost/SSOwat

commit 95ae0607d5547552e6ca4374394de91b3fe188ad

Avoid having to click two times on the tile to show the portal

portal/assets/js/ynh_portal.js
@@ -264,6 +264,7 @@ function init_portal_button_and_overlay()
   portalOverlay.setAttribute("id","ynh-overlay");
   portalOverlay.setAttribute("style","visibility: hidden;"); // make sure the overlay is invisible already when loading it
   document.body.insertBefore(portalOverlay, null);
+  Element.addClass(portalOverlay, 'ynh-fadeOut');
 
   // Inject portal button
   var portalButton = document.createElement('a');
@@ -279,8 +280,6 @@ function init_portal_button_and_overlay()
       // Prevent default click
       window.eventPreventDefault(e);
       // Toggle overlay on YNHPortal button click
-      Element.toggleClass(portalOverlay, 'visible');
-      Element.toggleClass(portalButton, 'visible');
       Element.toggleClass(document.querySelector('html'), 'ynh-panel-active');
       Element.toggleClass(portalOverlay, 'ynh-active');
 

commit a256762ae778708aefa545f845a939c78cfbaf0d

No idea what I'm doing, but this fixes the small blink issue introduced by 95ae060

portal/assets/js/ynh_portal.js
@@ -264,7 +264,6 @@ function init_portal_button_and_overlay()
   portalOverlay.setAttribute("id","ynh-overlay");
   portalOverlay.setAttribute("style","visibility: hidden;"); // make sure the overlay is invisible already when loading it
   document.body.insertBefore(portalOverlay, null);
-  Element.addClass(portalOverlay, 'ynh-fadeOut');
 
   // Inject portal button
   var portalButton = document.createElement('a');
@@ -283,7 +282,7 @@ function init_portal_button_and_overlay()
       Element.toggleClass(document.querySelector('html'), 'ynh-panel-active');
       Element.toggleClass(portalOverlay, 'ynh-active');
 
-      if (portalOverlay.classList.contains('ynh-active')) {
+      if (Element.hasClass(portalOverlay, 'ynh-active')) {
           meta_viewport.setAttribute('content', meta_viewport_content);
           Element.addClass(portalOverlay, 'ynh-fadeIn');
           Element.removeClass(portalOverlay, 'ynh-fadeOut');