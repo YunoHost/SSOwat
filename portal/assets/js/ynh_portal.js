38d87adbc0795498eee3bcbf1450f61a0d80a4ee
unknown <andrelauret@dttechnology.fr>
2021-05-12 02:54:32 +0400

    Deleting somes unused parts from last commit

--- portal/assets/js/ynh_portal.js
+++ portal/assets/js/ynh_portal.js
@@ -374,27 +374,3 @@ function tweak_portal_when_in_iframe()
         });
     }
 }
-
-var lastY = 0; // Needed in order to determine direction of scroll.
-$(".scroll-container").on('touchstart', function(event) {
-    lastY = event.touches[0].clientY;
-});
-
-$('.scroller').on('touchmove', function(event) {
-    var top = event.touches[0].clientY;
-
-    // Determine scroll position and direction.
-    var scrollTop = $(event.currentTarget).scrollTop();
-    var direction = (lastY - top) < 0 ? "up" : "down";
-
-    // FIX IT!
-    if (scrollTop == 0 && direction == "up") {
-      // Prevent scrolling up when already at top as this introduces a freeze.
-      event.preventDefault();
-    } else if (scrollTop >= (event.currentTarget.scrollHeight - event.currentTarget.outerHeight()) && direction == "down") {
-      // Prevent scrolling down when already at bottom as this also introduces a freeze.
-      event.preventDefault();
-    }
-
-    lastY = top;
-});
